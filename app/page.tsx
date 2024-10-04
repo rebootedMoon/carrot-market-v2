06dd32a2cd18640063a32154fa90ced42325d4cc
rebootedCoder2 <oborimoon@gmail.com>
2024-10-05 08:24:15 +0900

    state modifier- * or has()

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,13 @@
 export default function Home() {
   return (
   <main className="bg-gray-100  sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100  h-screen flex items-center justify-center p-5 ">
-    <div className="bg-white shadow-lg p-5 rounded-2xl w-full max-w-screen-sm  flex flex-col md:flex-row gap-2">
-      <input className="w-full rounded-full h-12 bg-gray-200 pl-5 outline-none ring ring-transparent focus:ring-green-500 focus:ring-offset-2 transition-shadow placeholder:text-red-600  placeholder:drop-shadow invalid:bg-red-200 invalid:focus:ring-orange-500 peer" 
+    <div className="bg-white shadow-lg p-5 rounded-2xl w-full max-w-screen-sm  flex flex-col md:flex-row gap-2 *:outline-none ring ring-transparent transition-shadow  has-[:invalid]:ring-red-100 ">
+      <input className="w-full rounded-full h-12 bg-gray-200 pl-5  ring ring-transparent focus:ring-green-500 focus:ring-offset-2 transition-shadow placeholder:text-red-600  placeholder:drop-shadow invalid:bg-red-200 invalid:focus:ring-orange-500 peer" 
       type="email" 
       required
       placeholder="Search here..."/>
       <span className="text-red-500 font-medium hidden peer-invalid:block"> Email is required</span>
-      <button className=" bg-black text-white py-2 rounded-full active:scale-90  transition-transform font-medium outline-none md:px-10 peer-invalid:bg-gray-400">Search</button>
+      <button className=" bg-black text-white py-2 rounded-full active:scale-90  transition-transform font-medium  md:px-10 peer-invalid:bg-gray-400">Search</button>
     </div>
   </main>
   );
