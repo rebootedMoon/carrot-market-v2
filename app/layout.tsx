c339a796bb169631c14a85931382a814e2256462
rebootedCoder2 <oborimoon@gmail.com>
2024-10-06 00:24:18 +0900

    group

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import {Inter} from "next/font/google";
 import "./globals.css";
+
+const inter = Inter({subsets:["latin"]});
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -26,7 +29,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.className} `}
       >
         {children}
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,12 @@ export default function Home() {
   <main className="bg-gray-100  sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100  h-screen flex items-center justify-center p-5 ">
     <div className="bg-white shadow-lg p-5 rounded-2xl w-full max-w-screen-sm  flex flex-col gap-4 ">
      {["Honey Jung", "Moon Joe", "Andy Bean", "Yourself"].map((person, index)=> 
-     <div key={index} className="flex  items-center gap-5">
+     <div key={index} className="flex  items-center gap-5 group">
       <div className="size-7 bg-blue-400 rounded-full " />
-      <span className="text-lg font-medium">{person}</span>
+      <span className="text-lg font-medium group-hover:text-red-500">{person}</span>
       <div className="size-6 rounded-full bg-red-500 text-white flex items-center justify-center relative">
         <span className="z-10">{index}</span>
-        <div className="size-6 rounded-full bg-red-500 absolute animate-ping" />
+        <div className="size-6 rounded-full bg-red-500 absolute animate-ping anima" />
       </div>
      </div>
     )}
