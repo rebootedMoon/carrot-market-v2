46d015dc3b936f2a6fd3942edc0a3f88d7cbf98a
rebootedCoder2 <oborimoon@gmail.com>
2024-10-05 18:39:20 +0900

    list & animation

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,17 @@
 export default function Home() {
   return (
   <main className="bg-gray-100  sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100  h-screen flex items-center justify-center p-5 ">
-    <div className="bg-white shadow-lg p-5 rounded-2xl w-full max-w-screen-sm  flex flex-col md:flex-row gap-2 *:outline-none ring ring-transparent transition-shadow  has-[:invalid]:ring-red-100 ">
-      <input className="w-full rounded-full h-12 bg-gray-200 pl-5  ring ring-transparent focus:ring-green-500 focus:ring-offset-2 transition-shadow placeholder:text-red-600  placeholder:drop-shadow invalid:bg-red-200 invalid:focus:ring-orange-500 peer" 
-      type="email" 
-      required
-      placeholder="Search here..."/>
-      <span className="text-red-500 font-medium hidden peer-invalid:block"> Email is required</span>
-      <button className=" bg-black text-white py-2 rounded-full active:scale-90  transition-transform font-medium  md:px-10 peer-invalid:bg-gray-400">Search</button>
+    <div className="bg-white shadow-lg p-5 rounded-2xl w-full max-w-screen-sm  flex flex-col gap-4 ">
+     {["Honey Jung", "Moon Joe", "Andy Bean", "Yourself"].map((person, index)=> 
+     <div key={index} className="flex  items-center gap-5">
+      <div className="size-7 bg-blue-400 rounded-full " />
+      <span className="text-lg font-medium">{person}</span>
+      <div className="size-6 rounded-full bg-red-500 text-white flex items-center justify-center relative">
+        <span className="z-10">{index}</span>
+        <div className="size-6 rounded-full bg-red-500 absolute animate-ping" />
+      </div>
+     </div>
+    )}
     </div>
   </main>
   );
