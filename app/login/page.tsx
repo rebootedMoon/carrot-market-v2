ee46ef3170cd33ec85bffb7f96afce36e0daeaec
rebootedCoder2 <oborimoon@gmail.com>
2024-10-06 09:54:26 +0900

    login screen

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,15 +2,14 @@ import FormButton from "@/components/form-btn";
 import FormInput from "@/components/form-input";
 import SocialLogin from "@/components/social-login";
 
-export default function CreateAccount() {
+export default function LogIn() {
   return (
     <div className="flex flex-col gap-10 p-5">
       <div className="flex flex-col gap-2 *:font-medium">
         <h1 className="text-2xl">안녕하세요!</h1>
-        <h2 className="text-xl">Fill in the form below to join</h2>
+        <h2 className="text-xl">Log in with email and password.</h2>
       </div>
       <form className="flex flex-col gap-3">
-        <FormInput type="text" placeholder="Username" required errors={[]} />
         <FormInput type="email" placeholder="Email" required errors={[]} />
         <FormInput
           type="password"
@@ -18,13 +17,8 @@ export default function CreateAccount() {
           required
           errors={[]}
         />
-        <FormInput
-          type="password"
-          placeholder="Confirm Password"
-          required
-          errors={[]}
-        />
-        <FormButton loading={false} text="Create account" />
+
+        <FormButton loading={false} text="Sign In" />
       </form>
       <SocialLogin />
     </div>
