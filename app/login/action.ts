3cb9d90d6f39f49cae0452ae92c460a35204e152
rebootedCoder2 <oborimoon@gmail.com>
2024-10-07 18:25:40 +0900

    useFormState

---
+++ app/login/action.ts
@@ -0,0 +1,9 @@
+"use server";
+
+// import { redirect } from "next/navigation";
+
+export async function handleForm(prevState: any, formData: FormData) {
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+  // redirect("/");
+  return { errors: ["wrong password", "password too short"] };
+}
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,23 +1,19 @@
+"use client";
 import FormButton from "@/components/form-btn";
 import FormInput from "@/components/form-input";
 import SocialLogin from "@/components/social-login";
-import { useFormStatus } from "react-dom";
+import { useFormState, useFormStatus } from "react-dom";
+import { handleForm } from "./action";
 
 export default function LogIn() {
-  const handleForm = async (formData: FormData) => {
-    "use server";
-    await new Promise((resolve) => setTimeout(resolve, 5000));
-    console.log("server 에서 실행된대");
-    console.log(formData.get("email"), formData.get("password"));
-  };
-
+  const [state, action] = useFormState(handleForm, null);
   return (
     <div className="flex flex-col gap-10 p-5">
       <div className="flex flex-col gap-2 *:font-medium">
         <h1 className="text-2xl">안녕하세요!</h1>
         <h2 className="text-xl">Log in with email and password.</h2>
       </div>
-      <form action={handleForm} className="flex flex-col gap-3">
+      <form action={action} className="flex flex-col gap-3">
         <FormInput
           name="email"
           type="email"
@@ -30,7 +26,7 @@ export default function LogIn() {
           type="password"
           placeholder="Password"
           required
-          errors={[]}
+          errors={state?.errors ?? []}
         />
         <FormButton text="Sign In" />
       </form>
