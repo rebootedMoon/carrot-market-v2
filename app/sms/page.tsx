4aa5f281a71838d015a8c4d63adc1684aa88622a
rebootedCoder2 <oborimoon@gmail.com>
2024-10-09 09:39:53 +0900

    prisma setup

--- app/sms/page.tsx
+++ app/sms/page.tsx
@@ -26,6 +26,7 @@ export default function SMSLogin() {
             required
             min={100000}
             max={999999}
+            errors={state.error?.formErrors}
           />
         ) : (
           <Input
