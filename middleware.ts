9bb5c69567325a48531635b87faff95d67e3832f
rebootedCoder2 <oborimoon@gmail.com>
2024-10-22 17:51:45 +0900

    matcher

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,21 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
+  console.log("hello");
+  const pathname = request.nextUrl.pathname;
+  if (pathname === "/") {
+    const response = NextResponse.next();
+    response.cookies.set("middleware-cookie", "hello!");
+    return response;
+  }
   if (request.nextUrl.pathname === "/profile") {
     return NextResponse.redirect(new URL("/", request.url));
   }
 }
+
+export const config = {
+  // matcher: ["/", "/profile", "/create-account", "/user/:path*" ],
+  matcher: [
+    "/((?!api | _next/static | _next/image | favicon.ico).*)",
+  ],
+};
