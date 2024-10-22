e78dd14cf28c08bb2f0cd8094f3d502506461942
rebootedCoder2 <oborimoon@gmail.com>
2024-10-22 21:13:54 +0900

    authentication middleware

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,31 @@
 import { NextRequest, NextResponse } from "next/server";
+import getSession from "./lib/session";
 
-export function middleware(request: NextRequest) {
-  console.log("hello");
-  const pathname = request.nextUrl.pathname;
-  if (pathname === "/") {
-    const response = NextResponse.next();
-    response.cookies.set("middleware-cookie", "hello!");
-    return response;
-  }
-  if (request.nextUrl.pathname === "/profile") {
-    return NextResponse.redirect(new URL("/", request.url));
+interface Routes {
+  [key: string]: boolean;
+}
+const publicOnlyUrls: Routes = {
+  "/": true,
+  "/login": true,
+  "/sms": true,
+  "/create-account": true,
+};
+export async function middleware(request: NextRequest) {
+  const session = await getSession();
+  const exists = publicOnlyUrls[request.nextUrl.pathname];
+  if (!session.id) {
+    //logout
+    if (!exists) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+  } else {
+    if (exists) {
+      return NextResponse.redirect(new URL("/products", request.url));
+    }
   }
 }
 
 export const config = {
   // matcher: ["/", "/profile", "/create-account", "/user/:path*" ],
-  matcher: [
-    "/((?!api | _next/static | _next/image | favicon.ico).*)",
-  ],
+  matcher: ["/((?!api|_next/static| _next/image|favicon.ico).*)"],
 };
