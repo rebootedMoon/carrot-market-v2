be7cd72246566ebfcef867ff77642932cd05bdf3
rebootedCoder2 <oborimoon@gmail.com>
2024-11-17 12:35:20 +0900

    pagination actions

--- app/(tabs)/products/page.tsx
+++ app/(tabs)/products/page.tsx
@@ -1,7 +1,9 @@
 import ListProduct from "@/components/list-product";
+import ProductList from "@/components/product-list";
 import db from "@/lib/db";
+import { Prisma } from "@prisma/client";
 
-async function getProducts() {
+async function getInitialProducts() {
   // await new Promise((resolve) => setTimeout(resolve, 10000));
   const products = await db.product.findMany({
     select: {
@@ -11,17 +13,23 @@ async function getProducts() {
       photo: true,
       id: true,
     },
+    take: 1,
+    orderBy: {
+      created_at: "desc",
+    },
   });
   return products;
 }
 
+export type InitialProducts = Prisma.PromiseReturnType<
+  typeof getInitialProducts
+>;
+
 export default async function Products() {
-  const products = await getProducts();
+  const initialProducts = await getInitialProducts();
   return (
-    <div className="p-5 flex flex-col gap-5">
-      {products.map((product) => (
-        <ListProduct key={product.id} {...product} />
-      ))}
+    <div>
+      <ProductList initialProducts={initialProducts} />
     </div>
   );
 }
---
+++ components/product-list.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { useState } from "react";
+import ListProduct from "./list-product";
+import { InitialProducts } from "@/app/(tabs)/products/page";
+import { getMoreProducts } from "@/app/(tabs)/products/actions";
+
+interface ProductListProps {
+  initialProducts: InitialProducts;
+}
+
+export default function ProductList({
+  initialProducts,
+}: ProductListProps) {
+  const [products, setProducts] = useState(initialProducts);
+  const [isLoading, setIsLoading] = useState(false);
+  const [page, setPage] = useState(0);
+  const [lastPage, setLastPage] = useState(false);
+
+  const onLoadMoreClick = async () => {
+    setIsLoading(true);
+    const newProducts = await getMoreProducts(page + 1);
+    if (newProducts.length !== 0) {
+      setPage((prev) => prev + 1);
+      setProducts((prev) => [...prev, ...newProducts]);
+    } else {
+      setLastPage(true);
+    }
+    setIsLoading(false);
+  };
+  return (
+    <div className="p-5 flex flex-col gap-5">
+      {products.map((product) => (
+        <ListProduct key={product.id} {...product} />
+      ))}
+      {lastPage ? (
+        "No more items"
+      ) : (
+        <button
+          onClick={onLoadMoreClick}
+          disabled={isLoading}
+          className="text-sm font-semibold bg-orange-500 w-fit mx-auto px-3 py-2 rounded-md hover:opacity-90 active:scale-95"
+        >
+          {isLoading ? "로딩 중..." : "Load More .."}
+        </button>
+      )}
+    </div>
+  );
+}
