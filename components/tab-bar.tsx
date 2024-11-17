27e8c361b647f1ac7b482cc80cf5520a932c7afe
rebootedCoder2 <oborimoon@gmail.com>
2024-11-17 17:32:42 +0900

    infinite scroll

--- components/tab-bar.tsx
+++ components/tab-bar.tsx
@@ -22,7 +22,7 @@ export default function TabBar() {
     <div
       className="fixed bottom-0 w-full mx-auto max-w-screen-md 
     grid grid-cols-5 border-neutral-600 border-t px-5 py-3
-    *:text-white"
+    *:text-white bg-neutral-800"
     >
       <Link
         href="/products"
